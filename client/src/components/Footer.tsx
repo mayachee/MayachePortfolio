124d3606f156c91b0b8fbabb98e6ca4223749e7b
mayachee <80540449+mayachee@users.noreply.github.com>
2025-12-19 19:27:07 +0100

    Update PDF links in Footer component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -39,11 +39,11 @@ const Footer = () => {
               <SiLinkedin className="h-5 w-5" />
             </a>
             <a  className="bg-gray-100 p-3 rounded-full hover:bg-gray-200 hover:scale-110 transition-all duration-300 text-black shadow-sm"
-              href="https://ik.imagekit.io/szcfr7vth/M_Yassine_Ayache_lebenslauf.pdf?updatedAt=1761311534841" target="_blank">
+              href="https://ik.imagekit.io/szcfr7vth/MohamedYassineAyache_Lebenslauf.pdf" target="_blank">
               Lebenslauf PDF
             </a>
             <a  className="bg-gray-100 p-3 rounded-full hover:bg-gray-200 hover:scale-110 transition-all duration-300 text-black shadow-sm"
-              href="https://ik.imagekit.io/szcfr7vth/Mohamed_Yassine_Ayache_Resume.pdf?updatedAt=1761238380221" target="_blank">
+              href="https://ik.imagekit.io/szcfr7vth/MohamedYassineAyache_Resume.pdf" target="_blank">
                 Resume PDF
             </a>
           </div>
